93aef1fd61450fb9d5941833ec6625b1c17d9402
Dayana Souza <dayanalvesouza@gmail.com>
2024-02-27 17:06:45 -0300

    feita alterações no código.

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-alert('Boas-vindas ao jogo do Número Secreto');
-let numeroSecreto = 4;
-console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else{
-    alert('O numero secreto era ' + numeroSecreto + ', mas você escolheu ' + chute);
-    //alert('Você errou :(');
+alert('Boas-vindas ao jogo do Número Secreto');
+let numeroSecreto = parent(Math.random() * 10 + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    if (chute == numeroSecreto) {
+        break;
+        
+    } else{
+        if (chute > numeroSecreto) {
+            alert(`O numero secreto é menor que ${chute}`);
+            //alert('Você errou :(');
+        } else {
+            alert(`O numero secreto é maior que ${chute}`);
+        }
+
+        tentativas++;
+    }
 }
 
-
-
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+/*
+if (tentativas > 1) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+} else {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+}
+*/
